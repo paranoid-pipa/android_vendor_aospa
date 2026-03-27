4377f542d6f437c19cd62bf39ce0c24dc9770146
NSR-XINE <nasironfire07@gmail.com>
2026-03-27 14:29:11 +0100

    checkout los
    Change-Id: I03605d712b06c57bde4000840dce4cf70c9bff39

---
+++ build/core/definitions.mk
@@ -0,0 +1,12 @@
+#
+# Lineage-specific macros
+#
+define uniq
+$(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
+endef
+
+# Include board/platform macros
+include vendor/aospa/build/core/utils.mk
+
+# Include default installed modules
+include vendor/aospa/build/core/default_installed_modules.mk
